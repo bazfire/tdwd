aa66439d1183227156076bb767f31fcbebacea19
Barry McClendon <bmcclendon@austin.utexas.edu>
2014-01-16 14:00:05 -0600

    Name database, add it to .gitignore

--- functional_tests.py
+++ functional_tests.py
@@ -55,6 +55,8 @@ class NewVisitorTest(unittest.TestCase): #
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        import time
+        time.sleep(10)
         self.fail("FINISH THE TEST!")        
         
         
